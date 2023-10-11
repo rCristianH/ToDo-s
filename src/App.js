27bd8b4fcc9184d44b9a76edf548ad7d49ff1e24
Cristian Ramirez <ccrhdbj@outlook.com>
2023-10-11 09:06:28 -0500

    React context

--- src/App.js
+++ src/App.js
@@ -1,85 +1,13 @@
 //@ts-check
 import React, { useState } from "react";
-
-import { useLocalStorage } from "./hooks/useLocalStorage";
 import AppUI from "./AppUI";
+import { TodoProvider } from "./ToDoContext";
 
 function App() {
-  const removeAccents = (text) => {
-    return text
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .normalize("NFC")
-      .toLowerCase();
-  };
-
-  const {
-    item: toDo,
-    saveItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage("Data_ToDo_V1", []);
-
-  const [searchValue, setSearchValue] = useState("");
-
-  const completedToDo = (text) => {
-    const newTodo = [...toDo];
-    const toDoIndex = newTodo.findIndex((todo) => todo.text === text);
-    newTodo[toDoIndex].completed = true;
-    saveTodos(newTodo);
-    sortToDo();
-    console.log("complete");
-  };
-  const sortToDo = () => {
-    console.log("ordenar");
-    const newTodo = [...toDo];
-    newTodo.sort((a, b) => {
-      if (a.completed && b.completed) return 1;
-      if (!a.completed && b.completed) return -1;
-      return 0;
-    });
-    saveTodos(newTodo);
-  };
-  const removeToDo = (text) => {
-    console.log("remover");
-    const newTodo = [...toDo];
-    const toDoIndex = newTodo.findIndex((todo) => todo.text === text);
-    if (newTodo.length === 1) {
-      newTodo.push({ text: "Crea un ToDo", completed: false });
-    }
-    newTodo.splice(toDoIndex, 1);
-    saveTodos(newTodo);
-  };
-
-  const filteredTodos = toDo.filter((todo) =>
-    removeAccents(todo.text).includes(removeAccents(searchValue))
-  );
-
-  const handleAddTodo = (text) => {
-    console.log("add");
-    if (text[0] !== text[0].toUpperCase()) {
-      text = text[0].toUpperCase() + text.slice(1);
-    }
-
-    saveTodos([...toDo, { text, completed: false }]);
-  };
-
-  const completedCount = toDo.filter((todo) => todo.completed).length;
-  const totalCount = toDo.length;
-
   return (
-    <AppUI
-      completedCount={completedCount}
-      totalCount={totalCount}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      filteredTodos={filteredTodos}
-      handleAddTodo={handleAddTodo}
-      completedToDo={completedToDo}
-      removeToDo={removeToDo}
-      loading={loading}
-      error={error}
-    />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
--- src/AppUI.js
+++ src/AppUI.js
@@ -1,71 +1,73 @@
 import "./App.css";
 import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
 import React from "react";
 import TodoItemLoading from "./TodoItem/TodoItemLoading";
+import { TodoContext } from "./ToDoContext";
 
-function AppUI({
-  completedCount,
-  totalCount,
-  searchValue,
-  setSearchValue,
-  filteredTodos,
-  handleAddTodo,
-  completedToDo,
-  removeToDo,
-  loading,
-  error,
-}) {
+function AppUI() {
   return (
     <div className="App">
-      <header className="App-header">
-        {loading && <h1 className="App-title">Estamos buscando tus ToDo`s</h1>}
-        {error && (
-          <h1>
-            Something went wrong...
-            {error}
-          </h1>
+      <TodoContext.Consumer>
+        {({ loading, error, completedCount, totalCount}) => (
+          <header className="App-header">
+            {loading && (
+              <h1 className="App-title">Estamos buscando tus ToDo`s</h1>
+            )}
+            {error && (
+              <h1>
+                Something went wrong...
+                {error}
+              </h1>
+            )}
+            {!loading && (
+              <TodoCounter completed={completedCount} total={totalCount} />
+            )}
+          </header>
         )}
-        {!loading && (
-          <TodoCounter completed={completedCount} total={totalCount} />
-        )}
-        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      </header>
+      </TodoContext.Consumer>
 
       <main className="App-main">
-        <TodoList>
-          {loading && <p>Wait a moment...</p>}
-          {loading && (
-            <>
-              <TodoItemLoading />
-              <TodoItemLoading />
-              <TodoItemLoading />
-              <TodoItemLoading />
-            </>
+        <TodoContext.Consumer>
+          {({ filteredTodos, completedToDo, removeToDo, loading, error }) => (
+            <TodoList>
+              {loading && <p>Wait a moment...</p>}
+              {loading && (
+                <>
+                  <TodoItemLoading />
+                  <TodoItemLoading />
+                  <TodoItemLoading />
+                  <TodoItemLoading />
+                </>
+              )}
+              {error && <p>Something went wrong...</p>}
+              {!loading &&
+                filteredTodos.map((todo) => (
+                  <TodoItem
+                    onComplete={() => {
+                      completedToDo(todo.text);
+                    }}
+                    onRemove={() => {
+                      removeToDo(todo.text);
+                    }}
+                    key={todo.text}
+                    text={todo.text}
+                    completed={todo.completed}
+                  />
+                ))}
+            </TodoList>
           )}
-          {error && <p>Something went wrong...</p>}
-          {!loading &&
-            filteredTodos.map((todo) => (
-              <TodoItem
-                onComplete={() => {
-                  completedToDo(todo.text);
-                }}
-                onRemove={() => {
-                  removeToDo(todo.text);
-                }}
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-              />
-            ))}
-        </TodoList>
+        </TodoContext.Consumer>
       </main>
       <footer className="App-footer">
-        <CreateTodoButton onAdd={() => handleAddTodo(searchValue)} />
+        <TodoContext.Consumer>
+          {({ handleAddTodo, searchValue }) => (
+            <CreateTodoButton onAdd={() => handleAddTodo(searchValue)} />
+          )}
+        </TodoContext.Consumer>
       </footer>
     </div>
   );
