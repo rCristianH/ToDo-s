766db87cf0bcb0158f104edcc3bb537813814443
Cristian Ramirez <ccrhdbj@outlook.com>
2023-10-17 12:57:50 -0500

    toggle modal

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import { TodoContext } from "../ToDoContext";
 
 function CreateTodoButton() {
-  const { handleAddTodo, searchValue } = React.useContext(TodoContext);
+  const { handleAddTodo, searchValue, setOpenModal } = React.useContext(TodoContext);
+
+  const handleClick = () => {
+    if (searchValue.trim() === "") {
+      alert("Error: El campo de búsqueda está vacío.");
+    } else {
+      setOpenModal(state => !state)
+      handleAddTodo(searchValue);
+    }
+  };
+
   return (
-    <button
-      onClick={() => handleAddTodo(searchValue)}
-      className="btn btn-primary"
-    >
+    <button onClick={handleClick} className="btn btn-primary">
       Crear Todo
     </button>
   );
